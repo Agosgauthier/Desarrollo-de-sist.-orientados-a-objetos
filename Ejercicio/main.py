bde244be944a0711837a0239e548cb9f9df20dbf
Agosgauthier <168487742+Agosgauthier@users.noreply.github.com>
2025-06-10 19:53:25 -0300

    Update main.py

--- Ejercicio/main.py
+++ Ejercicio/main.py
@@ -1,12 +1,10 @@
 from gestor_usuario import GestorUsuarios
 from usuario_builder import UsuarioBuilder 
 
-# Uso
 gestor = GestorUsuarios.instancia or GestorUsuarios()
 
 gestor.agregar(UsuarioBuilder().nombre("Agoss").correo("agoss@email.com").construir())
 gestor.agregar(UsuarioBuilder().nombre("Carlos").correo("carlos@email.com").construir())
 
-# Mostrar usuarios
 for usuario in gestor.usuarios:
     print(usuario)
